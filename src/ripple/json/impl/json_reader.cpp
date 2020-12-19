0422b4a394836e59c7a47b2b5b6884d645d650a0
Marlon Hanks <owner@kedia.co>
2020-12-19 01:05:17 -0500

    Update json_reader.cpp

--- src/ripple/json/impl/json_reader.cpp
+++ src/ripple/json/impl/json_reader.cpp
@@ -693,7 +693,7 @@ Reader::decodeNumber ( Token& token )
         if ( c < '0'  ||  c > '9' )
             return addError ( "'" + std::string ( token.start_, token.end_ ) + "' is not a number.", token );
 
-        if ( value >= threshold ) // expected 99.9999999% false
+        if ( value >= threshold ) { // expected 99.9999999% false
 			if ( value > threshold )
 				return decodeDouble ( token );
 			else
@@ -707,6 +707,7 @@ Reader::decodeNumber ( Token& token )
 					return decodeDouble(token);
 				//else we're good
 			}
+	}
 
         value = value * 10 + Value::UInt (c - '0');
     }
